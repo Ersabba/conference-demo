cc226cb15bc8c980ba17d331bdedb7f116feb231
Lorenzo Sabbatini <lorenzo.sabbatini@exprivia.com>
2020-08-16 16:34:29 +0200

    Generazione WAR da deployare su Container esterno

--- src/main/java/com/pluralsight/conferencedemo/ConferenceDemoApplication.java
+++ src/main/java/com/pluralsight/conferencedemo/ConferenceDemoApplication.java
@@ -2,9 +2,11 @@ package com.pluralsight.conferencedemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ConferenceDemoApplication {
+// SpringBootServletInitializer sostituisce il web.xml per la configurazione della web app
+public class ConferenceDemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConferenceDemoApplication.class, args);
